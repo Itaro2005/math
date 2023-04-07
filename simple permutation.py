99a8a78884281356e2d4798172514d718df88832
Itaro2005 <70619641+Itaro2005@users.noreply.github.com>
2023-04-06 22:17:54 -0300

    Update simple permutation.py

--- simple permutation.py
+++ simple permutation.py
@@ -3,13 +3,13 @@ import math
 p = p
 n = n
 
-def fatorial2(n):
+def fatorial(n):
         valor = 1
         for i in range(1, n-1):
             valor *= i
         return valor
 
-value = fatorial2(n)/10
+value = fatorial(n)/10
 
 
 print(value)
